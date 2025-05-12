753bede0760701d53beedde50e194aa96b3b25f4
Gabriel <rosenberg.gabriel@outlook.com>
2025-05-13 00:07:35 +0200

    chore: update backend packages and eslint config

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,11 +1,8 @@
-module.exports = {
-  env: {
-    node: true,
-  },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {},
-}
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
+])
