6f828d2b748cce047ef0f76b1f88c8af72ba74dc
Gabriel <rosenberg.gabriel@outlook.com>
2025-05-13 00:49:41 +0200

    feat: implement debounce functionality in TodoListForm connect backend with todo lists in FE

--- frontend/src/todos/components/TodoListForm.jsx
+++ frontend/src/todos/components/TodoListForm.jsx
@@ -1,59 +1,80 @@
 import { useState } from 'react'
 import { Card, CardContent, CardActions, Button, Typography } from '@mui/material'
-import { TodoListItem } from './TodoListItem.jsx'
 import AddIcon from '@mui/icons-material/Add'
+import useDebounce from '../../hooks/useDebounce.js'
+import { TodoListItem } from './TodoListItem.jsx'
 
 export const TodoListForm = ({ todoList, saveTodoList }) => {
   const [todos, setTodos] = useState(todoList.todos)
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    saveTodoList(todoList.id, { todos })
+  const handleSubmit = () => {
+    saveTodoList(todoList.id, todos)
+  }
+
+  const debounceEffect = useDebounce(handleSubmit)
+
+  const onTextfieldChange = (
+    /** @type {import("react").ChangeEvent<HTMLInputElement | HTMLTextAreaElement>} */ event,
+    /** @type {Todo} */ todo,
+    /** @type {number} */ index
+  ) => {
+    setTodos([
+      ...todos.slice(0, index),
+      {
+        ...todo,
+        description: event.target.value,
+      },
+      ...todos.slice(index + 1),
+    ])
+    debounceEffect()
+  }
+
+  const onCheckboxChange = (
+    /** @type {boolean} */ isChecked,
+    /** @type {Todo} */ todo,
+    /** @type {number} */ index
+  ) => {
+    const newTodos = [
+      ...todos.slice(0, index),
+      {
+        ...todo,
+        completed: isChecked,
+      },
+      ...todos.slice(index + 1),
+    ]
+    setTodos(newTodos)
+    saveTodoList(todoList.id, newTodos)
+  }
+
+  const onDelete = (/** @type {number} */ index) => {
+    const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)]
+    setTodos(newTodos)
+    saveTodoList(todoList.id, newTodos)
+  }
+
+  const onAddEmptyTodo = () => {
+    setTodos([...todos, { description: '' }])
   }
 
   return (
     <Card sx={{ margin: '0 1rem' }}>
       <CardContent>
         <Typography component='h2'>{todoList.title}</Typography>
-        <form
-          onSubmit={handleSubmit}
-          style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}
-        >
-          {todos.map((name, index) => (
+        <form style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+          {todos.map((/** @type {Todo} */ todo, /** @type {number} */ index) => (
             <TodoListItem
               key={index}
-              todo={{ description: name, completed: false }}
+              todo={todo}
               index={index}
-              onTextfieldChange={(event) => {
-                setTodos([
-                  // immutable update
-                  ...todos.slice(0, index),
-                  event.target.value,
-                  ...todos.slice(index + 1),
-                ])
-              }}
-              onCheckboxChange={() => {}}
-              onDelete={() => {
-                setTodos([
-                  // immutable delete
-                  ...todos.slice(0, index),
-                  ...todos.slice(index + 1),
-                ])
-              }}
+              onTextfieldChange={onTextfieldChange}
+              onCheckboxChange={onCheckboxChange}
+              onDelete={onDelete}
             />
           ))}
           <CardActions>
-            <Button
-              type='button'
-              color='primary'
-              onClick={() => {
-                setTodos([...todos, ''])
-              }}
-            >
-              Add Todo <AddIcon />
-            </Button>
-            <Button type='submit' variant='contained' color='primary'>
-              Save
+            <Button type='button' color='primary' onClick={onAddEmptyTodo}>
+              Add Todo
+              <AddIcon />
             </Button>
           </CardActions>
         </form>
